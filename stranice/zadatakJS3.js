5225d4ec5815c42b1ceaa6b6164b99611b481a00
programer10 <programer10@ucione.local>
2023-02-20 18:54:29 +0100

    JS zadaci, for petlja

---
+++ stranice/zadatakJS3.js
@@ -0,0 +1,16 @@
+var firstName = "John";
+var lastName = "Doe";
+
+var fullName = `full name = ${firstName} ${lastName}`; //fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+var x = z === 2? y : 5; //ternarni zapis
+
+//raspisan 
+
+if (z === 2) {
+    x = y;
+} else {
+    x = 5;
+}
