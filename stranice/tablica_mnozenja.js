f1e4c37c94c9c73d737974ea4077b60a279204c0
programer10 <programer10@ucione.local>
2023-03-02 19:17:36 +0100

    DOM struktura i manipuliranje

---
+++ stranice/tablica_mnozenja.js
@@ -0,0 +1,9 @@
+console.log("Tablica množenja:", "\n");
+
+for (mnozenik = 1; mnozenik <= 10; mnozenik++) {
+    for (mnozitelj = 1; mnozitelj <= 10; mnozitelj++){
+        var rezultat = mnozenik * mnozitelj;
+        console.log(mnozenik + " * " + mnozitelj + " = " + rezultat);
+    }
+    console.log("-----------------");
+}
